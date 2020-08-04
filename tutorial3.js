6c4e9fb77f90acfd93745e47546e00bda26e73b3
sujal111 <62564807+sujal111@users.noreply.github.com>
2020-08-04 10:15:23 -0700

    Add files via upload

---
+++ tutorial3.js
@@ -0,0 +1,16 @@
+var person = function() {
+    console.log("This is an anonymous function")
+}
+person();
+
+
+//Anonymous Function
+
+var users = function() {
+    console.log("This is an arrow function");
+}
+users();
+
+//Arrow Function
+var users = () => console.log("This is an arrow functions");
+users();
